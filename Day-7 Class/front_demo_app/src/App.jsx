f09057097e08760ada07fdd87ea92d450d7de0d8
ASHOKA M <ashokashokm2001@gmail.com>
2026-03-18 20:10:05 +0530

    day 7 code and notes

---
+++ Day-7 Class/front_demo_app/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  let a = 10;
+  let b = 20;
+  return (
+    <>
+      <h1>Welcome to React JS</h1>
+      <h3>Addition of A and B : {a + b}</h3>
+    </>
+  );
+}
+
+export default App;
